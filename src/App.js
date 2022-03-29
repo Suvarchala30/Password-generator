8cb13309f525a71bb9270a4d3d41dbff58fdd106
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-03-29 19:42:33 +0530

    pg start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [lengthvalue,setLengthvalue]=useState(10)
+  const handleLength = (e)=>{
+    setLengthvalue(e.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="main-block">
+        <div className="output">
+
+        </div>
+        <div className="input-block">
+          <div className="len-block">
+          <label htmlFor="password-strength">Password Length</label>
+        <input type="number" min="8" className="length" value={lengthvalue} onChange={handleLength} id="password-strength"/>
+        </div>
+        <div className="check1-block">
+        <label htmlFor="check1" className="label1">Include Numbers</label>
+        <input type="checkbox" id="check1" value="Include-Numbers" /> 
+        </div>
+        <div className="check2-block">
+        <label htmlFor="check2" className="label1">Include symbols</label>
+        <input type="checkbox" id="check2" value="Include-symbols" /> 
+        </div>
+        <div className="check3-block">
+        <label htmlFor="check3" className="label1">Include uppercase</label>
+        <input type="checkbox" id="check3" value="Include-uppercase" /> 
+        </div>
+        <div className="check4-block">
+        <label htmlFor="check4" className="label1">Include lowercase</label>
+        <input type="checkbox" id="check4" value="Include-lowercase" /> 
+        </div>
+        <button className="submit-btn">Generate Password</button>
+        </div>
+      </form>
     </div>
   );
 }
